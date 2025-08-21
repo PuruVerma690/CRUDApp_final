ee4cd1413ba25b60b4d2bb5032f85b6545fdc754
PuruVerma690 <purook1010@gmail.com>
2025-08-21 22:25:14 +0530

    /dist

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+import dotenv from "dotenv";
+dotenv.config();
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
 });
 
 export const listItems = () => api.get("/items").then((r) => r.data);
